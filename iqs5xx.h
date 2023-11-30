7c61469260c23fd77a5fb7d3cdbbd01aef3a0f84
pragun <pragun.goyal@gmail.com>
2023-11-30 09:02:41 -0500

    Cleans up Azoteq i2c code

--- iqs5xx.h
+++ iqs5xx.h
@@ -7,6 +7,8 @@
 #define CHECK_BIT(a,i) ((a & (0b1 << i)) != 0)
 #define READ_CYCLE_TIME //For now this seems important for the IQS5XX to work properly
 
+extern uint32_t time_us_32();
+
 namespace IQS5XX {
 	const uint8_t address = 0b1110100;
 
@@ -19,8 +21,6 @@ namespace IQS5XX {
     const uint8_t MaxTouches = 5;
     const uint16_t PrevCycleTime = 0x000C;
     const uint16_t NumFingers = 0x0011;
-    //const uint16_t PrevCycleTime = 0x0C00;
-    //const uint16_t NumFingers = 0x1100;
     const uint16_t AbsoluteX_Base = 0x0016;	//(READ) 2 BYTES	//ADD 0x0007 FOR FINGER 2; 0x000E FOR FINGER 3; 0x0015 FOR FINGER 4 AND 0x001C FOR FINGER 5
     const uint16_t AbsoluteY_Base = 0x0018;	//(READ) 2 BYTES	//ADD 0x0007 FOR FINGER 2; 0x000E FOR FINGER 3; 0x0015 FOR FINGER 4 AND 0x001C FOR FINGER 5	 
     const uint16_t TouchStrength_Base = 0x001A;	//(READ) 2 BYTES	//ADD 0x0007 FOR FINGER 2; 0x000E FOR FINGER 3; 0x0015 FOR FINGER 4 AND 0x001C FOR FINGER 5
@@ -38,37 +38,83 @@ public:
     struct touch_xy_t{
         uint16_t x;
         uint16_t y;
+        uint16_t strength;
+        uint8_t area;
     };
 
     uint16_t num_touches;
     touch_xy_t touch_xy[IQS5XX::MaxTouches];
     uint16_t cycle_time = 0;
+    uint32_t last_read_time = 0;
 
-    I2C_Status ReadTouchData(){
+    inline I2C_Status ReadTwoBytes(uint16_t addr, uint8_t* data){
+        I2C_Status drv_status;
+        drv_status = this->write_n_then_read_m((uint8_t*) &addr,2,(uint8_t*) data,2,true,true);
+        return drv_status;
+    };
+
+    inline I2C_Status ReadByte(uint16_t addr, uint8_t* data){
+        I2C_Status drv_status;
+        drv_status = this->write_n_then_read_m((uint8_t*) &addr,2,(uint8_t*) data,1,true,true);
+        return drv_status;
+    };
+
+    I2C_Status ReadDataAlt(){
         uint8_t end_comm[3] = {0xEE,0xEE,0xEE};
         uint16_t addr = 0;
         uint16_t naddr = 0;
         I2C_Status drv_status;
         num_touches = 0;
         //this->start_transaction();
+        last_read_time = time_us_32();
+        uint8_t data[64];
+        uint8_t n = 46;
+        uint16_t reg = IQS5XX::PrevCycleTime;
 
+        for(uint8_t i = 0; i<n; i++){
+            drv_status = this->write_n_then_read_m((uint8_t*) &reg, 2, &(data[i]), 1 ,true,true);
+            printf("%x ",data[i]);
+            reg ++;
+        }
+        printf("\n");
+
+        return drv_status;
+    };
+
+    I2C_Status ReadSingleTouchData(uint8_t i, touch_xy_t& touch_xy){
+        uint16_t addr = 0;
+        I2C_Status drv_status;
+        addr = IQS5XX::FingerStartingMem[i] + IQS5XX::AbsoluteX_Base;
+        drv_status = ReadTwoBytes(addr,(uint8_t*) &touch_xy.x);
+
+        addr = IQS5XX::FingerStartingMem[i] + IQS5XX::AbsoluteY_Base;
+        drv_status = ReadTwoBytes(addr,(uint8_t*) &touch_xy.y);
+
+        addr = IQS5XX::FingerStartingMem[i] + IQS5XX::TouchStrength_Base;
+        drv_status = ReadTwoBytes(addr,(uint8_t*) &touch_xy.strength);
+
+        addr = IQS5XX::FingerStartingMem[i] + IQS5XX::Area_Base;
+        drv_status = ReadByte(addr,(uint8_t*) &touch_xy.area);
+        return drv_status;
+    }
+
+    I2C_Status ReadTouchData(){
+        uint8_t end_comm[3] = {0xEE,0xEE,0xEE};
+        uint16_t addr = 0;
+        I2C_Status drv_status;
+        num_touches = 0;
+        //this->start_transaction();
+        last_read_time = time_us_32();
 #ifdef READ_CYCLE_TIME
-        drv_status = this->write_n_then_read_m((uint8_t*) &IQS5XX::PrevCycleTime,2,(uint8_t*) &cycle_time,1,true,true);
+        drv_status = ReadByte(IQS5XX::PrevCycleTime,(uint8_t*) &cycle_time);
 #endif
-        drv_status = this->write_n_then_read_m((uint8_t*) &IQS5XX::NumFingers,2,(uint8_t*) &num_touches,1,true,true);
+        drv_status = ReadByte(IQS5XX::NumFingers,(uint8_t*) &num_touches);
         if((num_touches <= 5) && (num_touches >= 1)){
             for(uint8_t i = 0; i<num_touches; i++){
-                addr = IQS5XX::FingerStartingMem[i] + IQS5XX::AbsoluteX_Base;
-                naddr = (addr & 0xFF00) >> 8;
-                naddr |= (addr & 0x00FF) << 8;
-                drv_status = this->write_n_then_read_m((uint8_t*) &addr,2,(uint8_t*) &touch_xy[i].x,2,true,true);
-
-                addr = IQS5XX::FingerStartingMem[i] + IQS5XX::AbsoluteY_Base;
-                naddr = (addr & 0xFF00) >> 8;
-                naddr |= (addr & 0x00FF) << 8;
-                drv_status = this->write_n_then_read_m((uint8_t*) &addr,2,(uint8_t*) &touch_xy[i].y,2,true,true);
+                ReadSingleTouchData(i,touch_xy[i]);
             }
         }
+
         drv_status = this->write_n_bytes(end_comm,3);
         //this->end_transaction();
 
